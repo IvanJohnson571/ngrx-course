8de31dfe136ec28d5bf533e78524e598e948ee01
IvanJohnson571 <ivan_johnson2005@yahoo.com>
2024-10-17 14:04:10 +0300

    added logout logic with localstorage and ngrx.

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,20 +1,41 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { AuthActions } from "./action-types";
+import { tap } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class AuthEffects {
 
-    constructor(private astions$: Actions) {
+    login$ = createEffect(() =>
 
-        astions$.subscribe(action => {
+        this.astions$
+            .pipe(
+                ofType(AuthActions.login),
+                tap(action => localStorage.setItem(
+                    'user',
+                    JSON.stringify(action.user)
+                )
+                )
+            )
+        ,
+        { dispatch: false }
 
-            if (action.type == '[Login Page] User Login') {
-                localStorage.setItem('user', JSON.stringify(action["user"]))
+    );
 
-            }
+    logout$ = createEffect(() =>
+        this.astions$
+            .pipe(
+                ofType(AuthActions.logout),
+                tap(action => {
+                    localStorage.removeItem('user');
+                    this.router.navigateByUrl('/login');
+                })
+            )
+        ,
+        { dispatch: false }
+    );
 
-        });
-
-    }
+    constructor(private astions$: Actions, private router: Router) { }
 
 }
